ad837e21e59378583d38ffdb1e2c791101d6635d
jugnuprjpt <jugnuprjpt@gmail.com>
2026-04-23 16:36:58 +0530

    [MINOR]

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const BASE_URL = 'https://180-dhandhar-samaj.vercel.app' || 'http://localhost:5000';
 
 export const API_ENDPOINTS = {
     AUTH: {
